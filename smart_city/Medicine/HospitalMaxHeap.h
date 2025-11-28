88ddf061055e8c08ee08b881238db7b57f4ef18f
snow <muhbilalchaudhry@gmail.com>
2025-11-28 15:34:10 +0500

    hospital max heap

--- smart_city/Medicine/HospitalMaxHeap.h
+++ smart_city/Medicine/HospitalMaxHeap.h
@@ -11,46 +11,112 @@ public:
     HeapNode(string id, int bed) : hospitalId(id), bedNumber(bed) {}
 };
 
-class MinHeap {
+class MaxHeap {
 
-    void swap() {}
+    void swap(int i, int j) {
+        HeapNode* temp = heapArray[i];
+        heapArray[i] = heapArray[j];
+        heapArray[j] = temp;
+    }
 
-    void maxHeapify() {}
+    void maxHeapify(int i) {
+        int l = leftChild(i);
+        int r = rightChild(i);
+        int largest = i;
+    
+        if (l < size && heapArray[l]->bedNumber > heapArray[largest]->bedNumber) {
+            largest = l;
+        }
+        if (r < size && heapArray[r]->bedNumber > heapArray[largest]->bedNumber) {
+            largest = r;
+        }
+
+        if (largest != i) {
+            swap(i, largest);
+            maxHeapify(largest);
+        }
+
+    }
 
     int parent(int i) { return (i - 1) / 2; }
     int leftChild(int i) { return (2 * i + 1); }
     int rightChild(int i) { return (2 * i + 2); }
+
+    void resize() {
+        int newCap = capacity * 2;
+        HeapNode** newArray = new HeapNode * [newCap];
+
+        for (int i = 0; i < newCap; ++i) {
+            newArray[i] = nullptr;
+        }
+
+        for (int i = 0; i < size; ++i) {
+            newArray[i] = heapArray[i];
+        }
+
+        for (int i = 0; i < size; ++i) {
+            delete heapArray[i];
+        }
+        delete[] heapArray;
+
+        heapArray = newArray;
+        capacity = newCap;
+    }
 
 public:
     HeapNode** heapArray;
     int capacity;
     int size;
 
-    MinHeap(int cap) : capacity(cap), size(0) {
+    MaxHeap() : capacity(0), size(0), heapArray(nullptr) {}
+
+    void setTable(int cap) {
+        capacity = cap;
+        size = 0;
         heapArray = new HeapNode * [capacity];
         for (int i = 0; i < capacity; i++) {
             heapArray[i] = nullptr;
         }
     }
 
-    void insert(int stopId, int distance) {
+    void insert(string hospitalId, int bedNumber) {
+        if (size == capacity) {
+            resize();
+        }
+        size++;
+        int i = size - 1;
+        heapArray[i] = new HeapNode(hospitalId, bedNumber);
 
+        while (i != 0 && heapArray[parent(i)]->bedNumber < heapArray[i]->bedNumber) {
+            swap(i, parent(i));
+            i = parent(i);
+        }
     }
 
-    HeapNode* extractMin() {
+    HeapNode* getMax() const {
+        if (size > 0) {
+            return heapArray[0];
+        }
         return nullptr;
     }
 
-    void decreaseKey(int stopId, int newDist) {
-
+    void updateBedCount(int bedsRequired, string& hospitalId) {
+        int oldBedNumber = heapArray[0]->bedNumber;
+        if (bedsRequired > oldBedNumber) {
+            return;
+        }
+        int newBedNumber = oldBedNumber - bedsRequired;
+        hospitalId = heapArray[0]->hospitalId;
+        heapArray[0]->bedNumber = newBedNumber;
+        maxHeapify(0);
     }
 
-    bool isEmpty() {
+    bool isEmpty() const {
         return size == 0;
     }
 
     // Destructor
-    ~MinHeap() {
+    ~MaxHeap() {
         for (int i = 0; i < size; ++i) {
             delete heapArray[i];
         }
