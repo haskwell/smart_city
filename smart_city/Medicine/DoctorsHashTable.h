731a913441209f7aaa37743f36e029b10fa05931
syedmoin27 <syedmoin5408@gmail.com>
2025-11-28 14:51:10 +0500

    implemented Doctor, Patient and Medicine Hashing.

--- smart_city/Medicine/DoctorsHashTable.h
+++ smart_city/Medicine/DoctorsHashTable.h
@@ -24,16 +24,86 @@ public:
         }
     }
 
-    int hash(string busNo) { return 0; }
+	// Simple hash function based on the doctor's name using polynomial hashing
+    int hash(string doc) { 
+        
+		long long hashValue = 0;
+		int primeNumber = 17; // to reduce collisions
+		long long addNumber = 10; // polynomial base
 
-    void insert(Person b) {}
+        for (int i = 0; i < doc.length(); i++)
+        {
+            hashValue = (hashValue * primeNumber + doc[i]) % tableSize;
 
-    Person* search(string busNo) { return 0; }
+            //hashValue += ((docName[i] - 'a' + 26) * addNumber) % tableSize;
 
-    ~DoctorsHashTable() {
+			//addNumber = (addNumber * primeNumber) % tableSize;
+
+        }
+
+        return hashValue;
+    }
+
+    //insert doctor at empty space using hash value
+    void insert(Person doc) {
+        int index = hash(doc.name);
+
+        DoctorNode* docNode = new DoctorNode(doc);
+
+        if (table[index] == nullptr)
+        {
+            table[index] = docNode;
+        }
+
+        else //linked list
+        {
+            DoctorNode* temp = table[index];
+
+            while (temp->next != nullptr)
+            {
+                temp = temp->next;
+            }
+
+            temp->next = docNode;
+        }
+    }
+
+    Person* search(string doc) {
+        int index = hash(doc);
+
+        DoctorNode* current = table[index];
+
+        while (current != nullptr)
+        {
+            if (current->data.name == doc)
+            {
+                return &(current->data);
+            }
+            current = current->next;
+
+        }
+        return nullptr;
+    }
+
+    /*~DoctorsHashTable() {
         for (int i = 0; i < tableSize; i++) {
             delete[]table[i];
         }
         delete[]table;
+    }*/
+
+    ~DoctorsHashTable() {
+        for (int i = 0; i < tableSize; i++) 
+        {
+            DoctorNode* current = table[i];
+            // Traverse the linked list and delete every node individually
+            while (current != nullptr) {
+                DoctorNode* prev = current;
+                current = current->next;
+                delete prev;
+            }
+        }
+        // Finally, delete the array of pointers
+        delete[] table;
     }
 };
--- smart_city/Medicine/PatientHashTable.h
+++ smart_city/Medicine/PatientHashTable.h
@@ -24,16 +24,61 @@ public:
         }
     }
 
-    int hash(string busNo) { return 0; }
+    // Polynomial hash function based on patient name
+    int hash(string patientName) {
+        long long hashValue = 0;
+        int primeNumber = 17; // prime base
 
-    void insert(Person b) {}
+        for (int i = 0; i < patientName.length(); i++) {
+            hashValue = (hashValue * primeNumber + patientName[i]) % tableSize;
+        }
 
-    Person* search(string busNo) { return 0; }
+        return hashValue;
+    }
 
+    // Insert a patient into the table
+    void insert(Person patient) {
+        int index = hash(patient.name);
+
+        PatientNode* patientNode = new PatientNode(patient);
+
+        if (table[index] == nullptr) {
+            table[index] = patientNode;
+        }
+        else { // collision ? append to linked list
+            PatientNode* temp = table[index];
+            while (temp->next != nullptr) {
+                temp = temp->next;
+            }
+            temp->next = patientNode;
+        }
+    }
+
+    // Search a patient by name
+    Person* search(string patientName) {
+        int index = hash(patientName);
+
+        PatientNode* current = table[index];
+
+        while (current != nullptr) {
+            if (current->data.name == patientName) {
+                return &(current->data);
+            }
+            current = current->next;
+        }
+        return nullptr;
+    }
+
+    // Destructor to free memory
     ~PatientHashTable() {
         for (int i = 0; i < tableSize; i++) {
-            delete[]table[i];
+            PatientNode* current = table[i];
+            while (current != nullptr) {
+                PatientNode* prev = current;
+                current = current->next;
+                delete prev;
+            }
         }
-        delete[]table;
+        delete[] table;
     }
 };
